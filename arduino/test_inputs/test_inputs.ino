40ea996e6e5770ef72a5a6d7badb12466c6913b7
RJ-917 <rahul.jaswal@ugent.be>
2026-03-13 17:43:05 +0100

    test

--- arduino/test_inputs/test_inputs.ino
+++ arduino/test_inputs/test_inputs.ino
@@ -2,10 +2,15 @@ const int buttonPin = 2;
 const int ledPin = LED_BUILTIN;  
 
 int buttonState = 0;  
+
+// test 1
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
+
+//test 2
 
 void loop() {
   buttonState = digitalRead(buttonPin);
